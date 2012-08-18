a82f902b0c9aff8fe14978a0953fa3913d99beca
Fabien Chaillou <fabien.chaillou@gmail.com>
2012-08-18 16:40:26 -0400

    Initial commit

---
+++ src/com/github/fchaillou/tripmaker/TripList.java
@@ -0,0 +1,15 @@
+package com.github.fchaillou.tripmaker;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(value = R.layout.activity_trip_list)
+public class TripList extends SherlockFragmentActivity {
+
+	/*
+	 * @Override public boolean onCreateOptionsMenu(Menu menu) {
+	 * getMenuInflater().inflate(R.menu.activity_trip_list, menu); return true;
+	 * }
+	 */
+
+}
